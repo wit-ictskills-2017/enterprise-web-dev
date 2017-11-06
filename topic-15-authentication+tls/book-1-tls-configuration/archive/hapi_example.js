217a1f121298298b841c629879732bd409219d2d
Jimmy McGibney <jmcgibney@wit.ie>
2017-11-06 00:55:11 +0000

    minor edits

---
+++ topic-15-authentication+tls/book-1-tls-configuration/archive/hapi_example.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({port:4000});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
